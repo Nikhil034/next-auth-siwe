9a3867241c68aa4c44b1ca0ad53312116b44b4ca
purvik6062 <purvikpanchal6336@gmail.com>
2024-02-08 14:34:33 +0530

    timezone conversion from utc to local

--- src/components/DayTimePicker/DayTimeSchedule.js
+++ src/components/DayTimePicker/DayTimeSchedule.js
@@ -5,165 +5,200 @@ import { StyledTimePickerContainer } from "@/components/style components/StylesD
 import DayTimeScheduler from "@captainwalterdev/daytimescheduler";
 import { fakeRequest } from "./fakeRequest";
 import { useAccount } from "wagmi";
+import { DateTime, Duration } from "luxon";
 
 function DayTimeSchedule() {
-  const address = "0x3013bb4E03a7B81106D69C10710EaE148C8410E1";
-  const [isScheduling, setIsScheduling] = useState(false);
-  const [isScheduled, setIsScheduled] = useState(false);
-  const [scheduleErr, setScheduleErr] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-  const [APIData, setAPIData] = useState();
+    const { address } = useAccount();
+    // const address = "0x3013bb4E03a7B81106D69C10710EaE148C8410E1";
+    const [isScheduling, setIsScheduling] = useState(false);
+    const [isScheduled, setIsScheduled] = useState(false);
+    const [scheduleErr, setScheduleErr] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    const [APIData, setAPIData] = useState();
 
-  useEffect(() => {
-    getAvailability();
-  }, []);
-
-  const getAvailability = async () => {
-    try {
-      const response = await fetch(`/api/get-availability/${address}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      const result = await response.json();
-      console.log("result", result);
-      if (result.success) {
-        setAPIData(result.data);
-        setIsLoading(false); // Set loading to false once data is fetched
-      }
-    } catch (error) {
-      console.log("error in catch", error);
-    }
-  };
-
-  const handleScheduled = (date) => {
-    setIsScheduling(true);
-    setScheduleErr("");
-    console.log("date in handleScheduled", date);
-
-    fakeRequest(date)
-      .then((json) => {
-        setScheduleErr("");
-        setIsScheduled(true);
-        console.log("fake response: ", json);
-        setTimeout(() => {
-          setIsScheduled(false);
+    useEffect(() => {
+        const loadingTimeout = setTimeout(() => {
+            setIsLoading(false);
         }, 2000);
-      })
-      .catch((err) => {
-        setScheduleErr(err);
-      })
-      .finally(() => {
-        setIsScheduling(false);
-      });
-  };
 
-  const timeSlotSizeMinutes = 15;
-  let dateAndRanges = [];
-  let allowedDates = [];
+        getAvailability();
 
-  // Check if API data exists
-  if (APIData) {
-    // Extract dateAndRanges and allowedDates from API data
-    APIData.forEach((item) => {
-      dateAndRanges.push(...item.dateAndRanges); // Concatenate dateAndRanges arrays
-      allowedDates.push(...item.allowedDates); // Concatenate allowedDates arrays
-    });
+        return () => {
+            clearTimeout(loadingTimeout);
+        };
+    }, [address]);
 
-    // Convert date strings to Date objects
-    dateAndRanges.forEach((range) => {
-      range.date = new Date(range.date);
-    });
-  }
+    const getAvailability = async () => {
+        try {
+            const response = await fetch(`/api/get-availability/${address}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
 
-  function timeSlotValidator(slotTime, dateAndRanges) {
-    for (const { date, timeRanges } of dateAndRanges) {
-      for (const [startHour, startMinute, endHour, endMinute] of timeRanges) {
-        const startTime = new Date(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate(),
-          startHour,
-          startMinute,
-          0
-        );
-
-        const endTime = new Date(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate(),
-          endHour,
-          endMinute,
-          0
-        );
-
-        if (
-          slotTime.getTime() >= startTime.getTime() &&
-          slotTime.getTime() <= endTime.getTime()
-        ) {
-          return true; // Return true if the slot is valid for any allowed date and time range
+            const result = await response.json();
+            console.log("result", result);
+            if (result.success) {
+                setAPIData(result.data);
+                setIsLoading(false);
+            }
+        } catch (error) {
+            console.log("error in catch", error);
         }
-      }
+    };
+
+    const handleScheduled = (date) => {
+        setIsScheduling(true);
+        setScheduleErr("");
+        console.log("date in handleScheduled", date);
+
+        fakeRequest(date)
+            .then((json) => {
+                setScheduleErr("");
+                setIsScheduled(true);
+                console.log("fake response: ", json);
+                setTimeout(() => {
+                    setIsScheduled(false);
+                }, 2000);
+            })
+            .catch((err) => {
+                setScheduleErr(err);
+            })
+            .finally(() => {
+                setIsScheduling(false);
+            });
+    };
+
+    const timeSlotSizeMinutes = 15;
+    let dateAndRanges = [];
+    let allowedDates = [];
+
+    // Check if API data exists
+    if (APIData) {
+        console.log("APIData", APIData)
+        APIData.forEach((item) => {
+            console.log("item", item)
+            dateAndRanges.push(...item.dateAndRanges);
+            allowedDates.push(...item.allowedDates);
+        });
+
+        dateAndRanges.forEach((range) => {
+            console.log("range", range)
+            range.date = new Date(range.date);
+            range.formattedUTCTime_startTime = new Date(range.formattedUTCTime_startTime)
+            range.formattedUTCTime_endTime = new Date(range.formattedUTCTime_endTime)
+
+            console.log("range.formattedUTCTime_startTime", range.formattedUTCTime_startTime)
+            console.log("range.formattedUTCTime_endTime", range.formattedUTCTime_endTime)
+
+            const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: false };
+            const formattedStartTime = range.formattedUTCTime_startTime.toLocaleTimeString(undefined, timeOptions);
+            const formattedEndTime = range.formattedUTCTime_endTime.toLocaleTimeString(undefined, timeOptions);
+
+            console.log("formattedStartTime", formattedStartTime);
+            console.log("formattedEndTime", formattedEndTime);
+
+            range.utcTime_startTime = formattedStartTime;
+            range.utcTime_endTime = formattedEndTime;
+
+            // const formatFromUTCTime_startTime = DateTime.fromFormat(formattedStartTime, "HH:mm");
+            // const localTime_startTime = formatFromUTCTime_startTime.toFormat("HH:mm");
+            // console.log("localTime_startTime", localTime_startTime);
+            const [startHourTime, startMinuteTime] = formattedStartTime.split(":");
+
+            // const formatFromUTCTime_endTime = DateTime.fromFormat(formattedEndTime, "HH:mm");
+            // const localTime_endTime = formatFromUTCTime_endTime.toFormat("HH:mm");
+            // console.log("localTime_endTime", localTime_endTime);
+            const [endHourTime, endMinuteTime] = formattedEndTime.split(":");
+
+            console.log([startHourTime, startMinuteTime, endHourTime, endMinuteTime])
+
+            // range.timeRanges.forEach((time) => {
+            //     console.log("time", time)
+            // })
+
+            range.timeRanges = [[startHourTime, startMinuteTime, endHourTime, endMinuteTime]]
+        });
+
+
+        allowedDates = allowedDates.map(dateString => new Date(dateString));
     }
 
-    return false; // Return false if the slot is not valid for any allowed date and time range
-  }
+    // dateAndRanges.forEach((range) => {
+    //     const startTimeUTCtoLocal = new Date(range.formattedUTCTime_startTime);
+    //     const endTimeUTCtoLocal = new Date(range.formattedUTCTime_endTime);
 
-  // Log UTC and local times
-  // Log UTC and local times
-  dateAndRanges.forEach((range) => {
-    const startDateUTCString = `${range.date.toISOString().split("T")[0]}T${
-      range.utcTime_startTime
-    }:00Z`;
-    const endDateUTCString = `${range.date.toISOString().split("T")[0]}T${
-      range.utcTime_endTime
-    }:00Z`;
+    //     console.log("startTimeUTCtoLocal", startTimeUTCtoLocal);
+    //     console.log("endTimeUTCtoLocal", endTimeUTCtoLocal);
 
-    const startTimeUTC = new Date(startDateUTCString);
-    const endTimeUTC = new Date(endDateUTCString);
-    const startTimeLocal = startTimeUTC.toLocaleString();
-    const endTimeLocal = endTimeUTC.toLocaleString();
+    // });
 
-    console.log("UTC Time Start:", range.utcTime_startTime);
-    console.log("UTC Time End:", range.utcTime_endTime);
-    console.log("Local Time Start:", startTimeLocal);
-    console.log("Local Time End:", endTimeLocal);
-  });
+    function timeSlotValidator(slotTime, dateAndRanges) {
+        for (const { date, timeRanges } of dateAndRanges) {
+            for (const [startHour, startMinute, endHour, endMinute] of timeRanges) {
+                const startTime = new Date(
+                    date.getFullYear(),
+                    date.getMonth(),
+                    date.getDate(),
+                    startHour,
+                    startMinute,
+                    0
+                );
 
-  return (
-    <div className="" style={{ display: "flex" }}>
-      <StyledTimePickerContainer>
-        {isLoading ? (
-          <div style={{ padding: "1rem" }}>Loading...</div>
-        ) : (
-          <>
-            <DayTimeScheduler
-              allowedDates={allowedDates}
-              timeSlotSizeMinutes={timeSlotSizeMinutes}
-              isLoading={isScheduling}
-              isDone={isScheduled}
-              err={scheduleErr}
-              onConfirm={handleScheduled}
-              timeSlotValidator={(slotTime) =>
-                timeSlotValidator(slotTime, dateAndRanges)
-              }
-              dateAndRanges={dateAndRanges.map((range) => ({
-                ...range,
-                formattedUTCTime_startTime: new Date(
-                  range.utcTime_startTime
-                ).toLocaleString(),
-                formattedUTCTime_endTime: new Date(
-                  range.utcTime_endTime
-                ).toLocaleString(),
-              }))}
-            />
-          </>
-        )}
-      </StyledTimePickerContainer>
-    </div>
-  );
+                const endTime = new Date(
+                    date.getFullYear(),
+                    date.getMonth(),
+                    date.getDate(),
+                    endHour,
+                    endMinute,
+                    0
+                );
+
+                if (
+                    slotTime.getTime() >= startTime.getTime() &&
+                    slotTime.getTime() <= endTime.getTime()
+                ) {
+                    return true; // Return true if the slot is valid for any allowed date and time range
+                }
+            }
+        }
+
+        return false; // Return false if the slot is not valid for any allowed date and time range
+    }
+
+    return (
+        <div className="" style={{ display: "flex" }}>
+            <StyledTimePickerContainer>
+                {isLoading ? (
+                    <div style={{ padding: "1rem" }}>Loading...</div>
+                ) : (
+                    <>
+                        <DayTimeScheduler
+                            allowedDates={allowedDates}
+                            timeSlotSizeMinutes={timeSlotSizeMinutes}
+                            isLoading={isScheduling}
+                            isDone={isScheduled}
+                            err={scheduleErr}
+                            onConfirm={handleScheduled}
+                            timeSlotValidator={(slotTime) =>
+                                timeSlotValidator(slotTime, dateAndRanges)
+                            }
+                            dateAndRanges={dateAndRanges.map((range) => ({
+                                ...range,
+                                formattedUTCTime_startTime: new Date(
+                                    range.utcTime_startTime
+                                ).toLocaleString(),
+                                formattedUTCTime_endTime: new Date(
+                                    range.utcTime_endTime
+                                ).toLocaleString(),
+                            }))}
+                        />
+                    </>
+                )}
+            </StyledTimePickerContainer>
+        </div>
+    );
 }
 
 export default DayTimeSchedule;
