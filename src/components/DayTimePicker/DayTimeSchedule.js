e67127e438a965c783853eba2d21ce16b13711b7
purvik6062 <purvikpanchal6336@gmail.com>
2024-02-12 10:13:40 +0530

    booking slots

--- src/components/DayTimePicker/DayTimeSchedule.js
+++ src/components/DayTimePicker/DayTimeSchedule.js
@@ -6,27 +6,46 @@ import DayTimeScheduler from "@captainwalterdev/daytimescheduler";
 import { fakeRequest } from "./fakeRequest";
 import { useAccount } from "wagmi";
 import { DateTime, Duration } from "luxon";
+import dateFns from 'date-fns';
+import { useSession } from "next-auth/react";
 
 function DayTimeSchedule() {
-    const { address } = useAccount();
+    const { isConnected, address } = useAccount();
+    const { data: session, status } = useSession();
+    const loading = status === "loading";
     // const address = "0x3013bb4E03a7B81106D69C10710EaE148C8410E1";
     const [isScheduling, setIsScheduling] = useState(false);
     const [isScheduled, setIsScheduled] = useState(false);
     const [scheduleErr, setScheduleErr] = useState("");
     const [isLoading, setIsLoading] = useState(true);
     const [APIData, setAPIData] = useState();
+    const [bookedSlots, setBookedSlots] = useState([]);
 
     useEffect(() => {
         const loadingTimeout = setTimeout(() => {
             setIsLoading(false);
         }, 2000);
 
-        getAvailability();
+        if (isConnected) {
+            getAvailability();
+        }
+
+        // Retrieve bookedSlots from localStorage on component mount
+        const storedBookedSlots = localStorage.getItem('bookedSlots');
+        if (storedBookedSlots) {
+            // Parse the string representation of dates into Date objects
+            const parsedBookedSlots = JSON.parse(storedBookedSlots).map(dateString => new Date(dateString));
+            setBookedSlots(parsedBookedSlots);
+            console.log("parsedBookedSlots", parsedBookedSlots)
+        }
 
         return () => {
             clearTimeout(loadingTimeout);
         };
-    }, [address]);
+    }, [address, isConnected, session]);
+
+
+
 
     const getAvailability = async () => {
         try {
@@ -48,21 +67,27 @@ function DayTimeSchedule() {
         }
     };
 
-    const handleScheduled = (date) => {
+    const handleScheduled = date => {
         setIsScheduling(true);
-        setScheduleErr("");
-        console.log("date in handleScheduled", date);
+        setScheduleErr('');
 
         fakeRequest(date)
-            .then((json) => {
-                setScheduleErr("");
+            .then(json => {
+
+                console.log('fake response: ', json.scheduled);
+
+                // Add the scheduled date to the bookedSlots array
+                const newBookedSlots = [...bookedSlots, new Date(json.scheduled)];
+                setBookedSlots(newBookedSlots);
+
+                console.log("newBookedSlots", newBookedSlots)
+                console.log("bookedSlots", JSON.stringify(newBookedSlots))
+                // Store bookedSlots in localStorage
+                localStorage.setItem('bookedSlots', JSON.stringify(newBookedSlots));
+                setScheduleErr('');
                 setIsScheduled(true);
-                console.log("fake response: ", json);
-                setTimeout(() => {
-                    setIsScheduled(false);
-                }, 2000);
             })
-            .catch((err) => {
+            .catch(err => {
                 setScheduleErr(err);
             })
             .finally(() => {
@@ -76,28 +101,28 @@ function DayTimeSchedule() {
 
     // Check if API data exists
     if (APIData) {
-        console.log("APIData", APIData)
+        // console.log("APIData", APIData)
         APIData.forEach((item) => {
-            console.log("item", item)
+            // console.log("item", item)
             dateAndRanges.push(...item.dateAndRanges);
             allowedDates.push(...item.allowedDates);
         });
 
         dateAndRanges.forEach((range) => {
-            console.log("range", range)
+            // console.log("range", range)
             range.date = new Date(range.date);
             range.formattedUTCTime_startTime = new Date(range.formattedUTCTime_startTime)
             range.formattedUTCTime_endTime = new Date(range.formattedUTCTime_endTime)
 
-            console.log("range.formattedUTCTime_startTime", range.formattedUTCTime_startTime)
-            console.log("range.formattedUTCTime_endTime", range.formattedUTCTime_endTime)
+            // console.log("range.formattedUTCTime_startTime", range.formattedUTCTime_startTime)
+            // console.log("range.formattedUTCTime_endTime", range.formattedUTCTime_endTime)
 
             const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: false };
             const formattedStartTime = range.formattedUTCTime_startTime.toLocaleTimeString(undefined, timeOptions);
             const formattedEndTime = range.formattedUTCTime_endTime.toLocaleTimeString(undefined, timeOptions);
 
-            console.log("formattedStartTime", formattedStartTime);
-            console.log("formattedEndTime", formattedEndTime);
+            // console.log("formattedStartTime", formattedStartTime);
+            // console.log("formattedEndTime", formattedEndTime);
 
             range.utcTime_startTime = formattedStartTime;
             range.utcTime_endTime = formattedEndTime;
@@ -106,66 +131,95 @@ function DayTimeSchedule() {
             // const localTime_startTime = formatFromUTCTime_startTime.toFormat("HH:mm");
             // console.log("localTime_startTime", localTime_startTime);
             const [startHourTime, startMinuteTime] = formattedStartTime.split(":");
-
             // const formatFromUTCTime_endTime = DateTime.fromFormat(formattedEndTime, "HH:mm");
             // const localTime_endTime = formatFromUTCTime_endTime.toFormat("HH:mm");
             // console.log("localTime_endTime", localTime_endTime);
             const [endHourTime, endMinuteTime] = formattedEndTime.split(":");
 
-            console.log([startHourTime, startMinuteTime, endHourTime, endMinuteTime])
-
-            // range.timeRanges.forEach((time) => {
-            //     console.log("time", time)
-            // })
+            // console.log([startHourTime, startMinuteTime, endHourTime, endMinuteTime])
 
             range.timeRanges = [[startHourTime, startMinuteTime, endHourTime, endMinuteTime]]
         });
 
-
-        allowedDates = allowedDates.map(dateString => new Date(dateString));
+        allowedDates = [...new Set(dateAndRanges.flatMap(({ formattedUTCTime_startTime, formattedUTCTime_endTime }) => [formattedUTCTime_startTime, formattedUTCTime_endTime]))];
     }
 
-    // dateAndRanges.forEach((range) => {
-    //     const startTimeUTCtoLocal = new Date(range.formattedUTCTime_startTime);
-    //     const endTimeUTCtoLocal = new Date(range.formattedUTCTime_endTime);
 
-    //     console.log("startTimeUTCtoLocal", startTimeUTCtoLocal);
-    //     console.log("endTimeUTCtoLocal", endTimeUTCtoLocal);
+    // function timeSlotValidator(slotTime, dateAndRanges, bookedSlots) {
+    //     for (const { date, timeRanges } of dateAndRanges) {
+    //         for (const [startHour, startMinute, endHour, endMinute] of timeRanges) {
+    //             const startTime = new Date(
+    //                 date.getFullYear(),
+    //                 date.getMonth(),
+    //                 date.getDate(),
+    //                 startHour,
+    //                 startMinute,
+    //                 0
+    //             );
 
-    // });
+    //             const endTime = new Date(
+    //                 date.getFullYear(),
+    //                 date.getMonth(),
+    //                 date.getDate(),
+    //                 endHour,
+    //                 endMinute,
+    //                 0
+    //             );
 
-    function timeSlotValidator(slotTime, dateAndRanges) {
-        for (const { date, timeRanges } of dateAndRanges) {
-            for (const [startHour, startMinute, endHour, endMinute] of timeRanges) {
-                const startTime = new Date(
-                    date.getFullYear(),
-                    date.getMonth(),
-                    date.getDate(),
-                    startHour,
-                    startMinute,
-                    0
-                );
+    //             if (
+    //                 slotTime.getTime() >= startTime.getTime() &&
+    //                 slotTime.getTime() <= endTime.getTime()
+    //             ) {
+    //                 // Check if the slot is booked
+    //                 const isBooked = bookedSlots.some(bookedSlot => {
+    //                     return (
+    //                         dateFns.isSameDay(date, bookedSlot) &&
+    //                         slotTime.getHours() === bookedSlot.getHours() &&
+    //                         slotTime.getMinutes() === bookedSlot.getMinutes()
+    //                     );
+    //                 });
 
-                const endTime = new Date(
-                    date.getFullYear(),
-                    date.getMonth(),
-                    date.getDate(),
-                    endHour,
-                    endMinute,
-                    0
-                );
+    //                 // If the slot is not booked, return true
+    //                 if (!isBooked) {
+    //                     return true;
+    //                 }
+    //             }
+    //         }
+    //     }
 
-                if (
-                    slotTime.getTime() >= startTime.getTime() &&
-                    slotTime.getTime() <= endTime.getTime()
-                ) {
-                    return true; // Return true if the slot is valid for any allowed date and time range
+    //     return false;
+    // }
+
+
+    function timeSlotValidator(slotTime, dateAndRanges, bookedSlots) {
+        for (const { formattedUTCTime_startTime: startTime, formattedUTCTime_endTime: endTime } of dateAndRanges) {
+
+            // Check if slotTime is within the time range of startTime and endTime
+            if (
+                slotTime.getTime() >= startTime.getTime() &&
+                slotTime.getTime() <= endTime.getTime()
+            ) {
+                // Check if the slot is booked
+                const isBooked = bookedSlots.some(bookedSlot => {
+                    console.log("slotTime.getHours()", slotTime.getHours(), ":", slotTime.getMinutes())
+                    console.log("bookedSlot.getHours()", bookedSlot.getHours(), ":", bookedSlot.getMinutes())
+                    return (
+                        dateFns.isSameDay(startTime, bookedSlot) &&
+                        slotTime.getHours() === bookedSlot.getHours() &&
+                        slotTime.getMinutes() === bookedSlot.getMinutes()
+                    );
+                });
+
+                // If the slot is not booked, return true
+                if (!isBooked) {
+                    return true;
                 }
             }
         }
 
-        return false; // Return false if the slot is not valid for any allowed date and time range
+        return false;
     }
+
 
     return (
         <div className="" style={{ display: "flex" }}>
@@ -181,18 +235,7 @@ function DayTimeSchedule() {
                             isDone={isScheduled}
                             err={scheduleErr}
                             onConfirm={handleScheduled}
-                            timeSlotValidator={(slotTime) =>
-                                timeSlotValidator(slotTime, dateAndRanges)
-                            }
-                            dateAndRanges={dateAndRanges.map((range) => ({
-                                ...range,
-                                formattedUTCTime_startTime: new Date(
-                                    range.utcTime_startTime
-                                ).toLocaleString(),
-                                formattedUTCTime_endTime: new Date(
-                                    range.utcTime_endTime
-                                ).toLocaleString(),
-                            }))}
+                            timeSlotValidator={(slotTime) => timeSlotValidator(slotTime, dateAndRanges, bookedSlots)}
                         />
                     </>
                 )}
