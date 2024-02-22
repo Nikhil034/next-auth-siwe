e29da1f68783d46889721454333c2e41594ec88e
purvik6062 <purvikpanchal6336@gmail.com>
2024-02-22 10:30:08 +0530

    pixel increased

--- src/components/style components/StylesDayTimeSchedule.js
+++ src/components/style components/StylesDayTimeSchedule.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledTimePickerContainer = styled.div`
     margin: 0 auto;
     background-color: #f0fff;
-    border-radius: 8px;
+    border-radius: 9px;
     box-shadow: 0 2px 24px rgba(0, 0, 0, 0.1);
     height: fit-content
     li{
