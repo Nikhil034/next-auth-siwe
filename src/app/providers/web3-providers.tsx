5b345c1bb9b016369c2bcc8de7f2fd4bdb9bfc0e
Nikhil034 <nikhilsondarava342@gmail.com>
2024-06-15 19:51:19 +0530

    intialize commit

--- src/app/providers/web3-providers.tsx
+++ src/app/providers/web3-providers.tsx
@@ -3,7 +3,12 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { ReactNode } from "react";
 
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  connectorsForWallets,
+  Wallet,
+} from "@rainbow-me/rainbowkit";
 import {
   RainbowKitSiweNextAuthProvider,
   GetSiweMessageOptions,
@@ -13,6 +18,13 @@ import { SessionProvider } from "next-auth/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { mainnet, optimism, arbitrum } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
+import { coinbaseWallet } from "@rainbow-me/rainbowkit/wallets";
+import { sdk } from "./setup";
+
+// Create provider
+const provider = sdk.makeWeb3Provider({ options: "smartWalletOnly" });
+// Use provider
+const addresses = provider.request({ method: "eth_requestAccounts" });
 
 interface RainbowKitProviderProps {
   children: ReactNode;
@@ -24,11 +36,24 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
   [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
+const { wallets } = getDefaultWallets({
   appName: "Test App",
   projectId: "c52f63cb512b7b43a8724eae05cb5130",
   chains,
 });
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Smart Wallet",
+    wallets: [coinbaseWallet({ chains, appName: "Test" })],
+  },
+]);
+
+// const connector = coinbaseWallet({
+//   appName: 'My Wagmi App',
+//   preference: 'smartWalletOnly',
+// })
 
 const wagmiConfig = createConfig({
   autoConnect: true,
@@ -46,8 +71,7 @@ export default function Web3Provider(props: RainbowKitProviderProps) {
     <WagmiConfig config={wagmiConfig}>
       <SessionProvider>
         <RainbowKitSiweNextAuthProvider
-          getSiweMessageOptions={getSiweMessageOptions}
-        >
+          getSiweMessageOptions={getSiweMessageOptions}>
           <RainbowKitProvider chains={chains}>
             {props.children}
           </RainbowKitProvider>
